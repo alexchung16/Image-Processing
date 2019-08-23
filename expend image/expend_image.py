49746df0d7c5e58e3a54b34d8b9f62768ad88800
zyongan <yonganzhong@outlook.com>
2019-08-23 15:28:35 +0800

    新增图像平移操作

--- expend image/expend_image.py
+++ expend image/expend_image.py
@@ -28,17 +28,18 @@ class MultipleProcessingImage(object):
         self.image_index = 0
         self.process_method = {
                                 1: 'sizeImage',
-                                2: 'rotationImage',
-                                3: 'perspectiveTransformImage',
-                                4: 'noiseImage',
-                                5: 'blurImage',
-                                6: 'lightImage',
-                                7: 'contrastImage',
-                                8: 'sharpingImage',
-                                9: 'hueImage',
-                                10: 'saturationImage',
-                                11: 'cutImage',
-                                12: 'dpiImage'
+                                2: 'translationImage',
+                                3: 'rotationImage',
+                                4: 'perspectiveTransformImage',
+                                5: 'noiseImage',
+                                6: 'blurImage',
+                                7: 'lightImage',
+                                8: 'contrastImage',
+                                9: 'sharpingImage',
+                                10: 'hueImage',
+                                11: 'saturationImage',
+                                12: 'cutImage',
+                                13: 'dpiImage'
                              }
 
     def mkdirCheck(self, path, is_makdir=False):
@@ -104,6 +105,69 @@ class MultipleProcessingImage(object):
             # 缩小图片(shrink image)
             size_img = cv.resize(img, None, fx=0.5, fy=0.5, interpolation=cv.INTER_CUBIC)
         return size_img
+
+    def roiImage(self, src_img, rect):
+        """
+        提取感兴趣区域（Region Of Interest, ROI）
+        :param img: 源图像
+        :param rect: 感兴趣区域信息
+        :return:
+        """
+        x, y, w, h = rect
+        # 存储三通道信息
+        roi_img = src_img[int(y):int(y + h), int(x):int(x + w)]
+        return roi_img
+
+    def translationImage(self, src_img, trans_rate=0.2, direction=3):
+        """
+        图像平移
+        :param img: 源图像
+        :param trans_rate: 平移比率
+        :param direction: 移动方向 0：左上， 1：右上 2：左下 3：右下 4：随机方向
+        :return: 目标图像
+        """
+        # 控制平移方向
+        direction_x = 1
+        direction_y = 1
+
+        roi_weight = src_img.shape[0] * (1 - trans_rate)
+        roi_height = src_img.shape[1] * (1 - trans_rate)
+
+        rect_info = (0, 0, src_img.shape[0], src_img.shape[1])
+        # 选择平移方向
+        if direction == 0:
+            direction_x, direction_y = (-1, -1)
+        elif direction == 1:
+            direction_x, direction_y = (1, -1)
+        elif direction == 2:
+            direction_x, direction_y = (-1, 1)
+            rect_info = (0, src_img.shape[1] * trans_rate, roi_weight, roi_height)
+        elif direction == 3:
+            direction_x, direction_y = (1, 1)
+            rect_info = (src_img.shape[0] * trans_rate, src_img.shape[1] * trans_rate, roi_weight, roi_height)
+        elif direction == 4:
+            direction_x, direction_y = tuple(random.sample((-1, 1), 2))
+
+        # 选择兴趣区域尺寸
+        if (direction_x, direction_y) == (-1, -1):
+            rect_info = (0, 0, roi_weight, roi_height)
+        elif (direction_x, direction_y) == (1, -1):
+            rect_info = (src_img.shape[0] * trans_rate, 0, roi_weight, roi_height)
+        elif (direction_x, direction_y) == (-1, 1):
+            rect_info = (0, src_img.shape[1] * trans_rate, roi_weight, roi_height)
+        elif (direction_x, direction_y) == (1, 1):
+            rect_info = (src_img.shape[0] * trans_rate, src_img.shape[1] * trans_rate, roi_weight, roi_height)
+
+        # 平移矢量大小
+        distance_x = src_img.shape[0] * trans_rate * direction_x
+        distance_y = src_img.shape[1] * trans_rate * direction_y
+        # 构建仿射矩阵
+        M = np.float32([[1, 0, distance_x], [0, 1, distance_y]])
+        # 获取平移图片
+        translation_img = cv.warpAffine(src_img, M, (int(src_img.shape[0]), int(src_img.shape[1])))
+        # 提取感兴趣区域
+        translation_roi_img = self.roiImage(translation_img, rect_info)
+        return translation_roi_img
 
     def rotationImage(self, img):
         """
@@ -343,6 +407,8 @@ class MultipleProcessingImage(object):
             for index in process_index[:-1]:
                 if self.process_method[index] == 'sizeImage':
                     process_img = self.sizeImage(process_img)
+                elif self.process_method[index] == 'translationImage':
+                    process_img = self.translationImage(process_img, 0.1, 4)
                 elif self.process_method[index] == 'rotationImage':
                     process_img = self.rotationImage(process_img)
                 elif self.process_method[index] == 'perspectiveTransformImage':
