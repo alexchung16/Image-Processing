4f46067e6466063aaea2d65df3db4016650498ec
zyongan <yonganzhong@outlook.com>
2019-12-02 20:28:16 +0800

    add rar unpack operation

--- FileOperation/file_unpack.py
+++ FileOperation/file_unpack.py
@@ -8,13 +8,13 @@
 import os
 import zipfile
 import tarfile
+import rarfile
 import time
 
 zip_file = '/home/alex/Documents/datasets/dogs_vs_cat_separate/test/test1575272889.zip'
 tar_file = '/home/alex/Documents/datasets/dogs_vs_cat_separate/test/test1575277216.tar.gz'
+rar_file = '/home/alex/Documents/datasets/dogs_vs_cat_separate/test/test.rar'
 dst_dir = '/home/alex/Documents/datasets/dogs_vs_cat_separate/test'
-
-
 
 def show_info_zip(zip_file):
     """
@@ -49,15 +49,15 @@ def file_unpack_zip(zip_file_path, dst_path):
         return False
 
 
-def file_unpack_tar(zip_file_path, dst_path):
+def file_unpack_tar(tar_file_path, dst_path):
     """
-    extract file from a zip file
-    :param zip_path:
+    extract file from a tar file
+    :param tar_file_path:
     :param dst_path:
     :return:
     """
     try:
-        with tarfile.open(zip_file_path, mode='r:gz') as tar:
+        with tarfile.open(tar_file_path, mode='r:gz') as tar:
             tar.extractall(path=dst_path)
             print('All file unpack to '+dst_path)
             return True
@@ -66,6 +66,25 @@ def file_unpack_tar(zip_file_path, dst_path):
         return False
 
 
+def file_unpack_rar(rar_file_path, dst_path):
+    """
+    extract file from a rar file
+    :param rar_file_path:
+    :param dst_path:
+    :return:
+    """
+    # error => Unrar not installed? (rarfile.UNRAR_TOOL='unrar') ubuntu
+    # solve => sudo apt-get install rar unrar
+    try:
+        with rarfile.RarFile(rarfile=rar_file_path, mode='r') as rar:
+            rar.extractall(path=dst_path)
+            print('All file unpack to '+dst_path)
+            return True
+    except Exception as e:
+        print(e)
+        return False
+
 if __name__ == "__main__":
     # file_unpack_zip(zip_file, dst_dir)
-    file_unpack_tar(tar_file, dst_dir)
+
+    file_unpack_rar(rar_file, dst_dir)
